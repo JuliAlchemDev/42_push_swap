191be79d2c1af5babbda3e7e1a1a9ed7583fb719
Julia Alkhimova <devalchem@gmail.com>
2026-06-24 11:07:31 +0200

    feat: implement push operation and add stack_b

--- playgound.c
+++ playgound.c
@@ -41,6 +41,7 @@ int	main(void){
 
 // Replace hardcoded stack_a with argv input, print results
 
+/*
 #include <stdlib.h>
 
 int main(int argc, char **argv)
@@ -69,7 +70,7 @@ int main(int argc, char **argv)
 		printf("_ _\n");
 		printf("a b\n");
 
-/******************** EXEC SWAP A *******************/
+// ******************** EXEC SWAP A *******************
 
 		printf("Exec sa:\n");
 		swap(stack_a);
@@ -85,7 +86,101 @@ int main(int argc, char **argv)
 	}
 	return (0);
 }
+*/
 
+// Push top element from stack_out to stack_in
 
+void	push(int *stack_in, int *stack_out, int *size_in, int *size_out)
+{
+    int	i;
+
+    if (*size_out == 0)
+        return ;
+    i = *size_in;
+    while (i > 0)
+    {
+        stack_in[i] = stack_in[i - 1];
+        i--;
+    }
+    stack_in[0] = stack_out[0];
+    i = 0;
+    while (i < *size_out - 1)
+    {
+        stack_out[i] = stack_out[i + 1];
+        i++;
+    }
+    (*size_in)++;
+    (*size_out)--;
+}
+
+// Add stack_b and apply push operation three times
+
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	int *stack_a;
+	int *stack_b;
+	int size_a;
+	int size_b;
+	int i;
+
+	size_b = 0;
+	i = 0;
+	if (argc > 1)
+	{
+		size_a = argc - 1;
+		stack_a = (int *)malloc(size_a * sizeof(int));
+		if (!stack_a)
+			return (1);
+		else
+		{
+			printf("Init a and b:\n");
+			while(i < size_a)
+			{
+				stack_a[i] = atoi(argv[i+1]);
+				printf("%d\n", stack_a[i]);
+				i++;
+			}
+
+		}
+
+		printf("_ _\n");
+		printf("a b\n");
+
+// ******************** EXEC SWAP A *******************
+
+		printf("Exec sa:\n");
+		swap(stack_a);
+		i = 0;
+
+		while (i < size_a)
+		{
+			printf("%d\n", stack_a[i]);
+			i++;
+		}
+		printf("_ _\n");
+		printf("a b\n");
+
+// ******************** EXEC PUSH B *3 *******************
+		stack_b = (int *)malloc(size_a * sizeof(int));
+		printf("Exec pb pb pb:\n");
+		push(stack_b, stack_a, &size_b, &size_a);
+		push(stack_b, stack_a, &size_b, &size_a);
+		push(stack_b, stack_a, &size_b, &size_a);
+
+		i = 0;
+
+		while (i < size_a) // ⚠️ Caution ⚠️
+		{
+			printf("%d %d\n", stack_a[i], stack_b[i]);
+			i++;
+		}
+		printf("_ _\n");
+		printf("a b\n");
+	}
+
+	return (0);
+}
 
 
