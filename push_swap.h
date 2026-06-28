3d743e2a431029359525094afa29a50abeb1ddcd
Julia Alkhimova <devalchem@gmail.com>
2026-06-28 19:06:54 +0200

    refactor: add push_swap.h with operation function declarations

---
+++ push_swap.h
@@ -0,0 +1,19 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+// DELETE !
+# include <stdio.h>
+# include <stdlib.h>
+// DELETE !
+
+// Operation functions
+
+void	swap(int *stack);
+void	s_swap(int *stack_a, int *stack_b);
+void	push(int *stack_in, int *stack_out, int *size_in, int *size_out);
+void    rotate(int *stack, int size);
+void	rrotate(int *stack_a, int size_a, int *stack_b, int size_b);
+void	rvs_rotate(int *stack, int size);
+void	rrvs_rotate(int *stack_a, int size_a, int *stack_b, int size_b);
+
+#endif
