1334b96f33123a3492ebb3785c3cf565b7994552
Julia Alkhimova <devalchem@gmail.com>
2026-07-03 12:33:26 +0200

    refactor: use size_t for loop index in debug trace prints to avoid comparison warnings

--- srcs/insertion_sort.c
+++ srcs/insertion_sort.c
@@ -14,13 +14,13 @@ void	insertion_sort(t_stack *a)
 		count_rot = 0;
 		limit = b->size;
 // // ============================= Debug Traces =====================
-// 		printf("Stack A:");
-// 		for (int i = 0; i < a->size; i++)
-// 			printf(" %d", a->data[i]);
-// 		printf("\nStack B:");
-// 		for (int i = 0; i < b->size; i++)
-// 			printf(" %d", b->data[i]);
-// 		printf("\nTemp value: %d\n", temp);
+		// printf("Stack A:");
+		// for (size_t i = 0; i < a->size; i++)
+		// 	printf(" %d", a->data[i]);
+		// printf("\nStack B:");
+		// for (size_t i = 0; i < b->size; i++)
+		// 	printf(" %d", b->data[i]);
+		// printf("\nTemp value: %d\n", temp);
 // // ============================= Debug =====================
 		while(a->data[0] < b->data[0] && count_rot < limit)
 		{
