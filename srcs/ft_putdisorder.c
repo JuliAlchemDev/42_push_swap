ae305fffdbe7dfd2b7bb3cd36753d44b295d9007
Andrea Serio on Comet <andrea.serio90@gmail.com>
2026-07-03 18:30:39 +0200

    lint: norminette

--- srcs/ft_putdisorder.c
+++ srcs/ft_putdisorder.c
@@ -6,7 +6,7 @@
 /*   By: aserio <aserio@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 17:38:13 by aserio            #+#    #+#             */
-/*   Updated: 2026/07/03 18:15:51 by aserio           ###   ########.fr       */
+/*   Updated: 2026/07/03 18:30:10 by aserio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	ft_putdisorder(float d)
 	int	decimals;
 
 	ft_putstr_fd("[bench] disorder: ", 2);
-	ft_putnbr_fd((int) (d * 100), 2);
+	ft_putnbr_fd((int)(d * 100), 2);
 	ft_putchar_fd('.', 2);
-	decimals = ((int) (d * 10000)) % 100;
+	decimals = ((int)(d * 10000)) % 100;
 	if (decimals < 10)
 		ft_putchar_fd('0', 2);
 	ft_putnbr_fd(decimals, 2);
