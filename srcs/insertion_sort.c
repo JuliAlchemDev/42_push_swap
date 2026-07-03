a563913b6687aac0aa9cd5f056180dc06b6f20a3
Julia Alkhimova <devalchem@gmail.com>
2026-07-03 10:49:31 +0200

    feat: implement first version of insertion_sort
    Contains debug printf's, to be removed before final submission

--- srcs/insertion_sort.c
+++ srcs/insertion_sort.c
@@ -16,32 +16,65 @@ static t_stack	*get_stack(int size, char **nums)
 		return (NULL);
 	return (stack);
 }
+
 // delete when finish
-void insertion_sort(t_stack *a)
+
+void	insertion_sort(t_stack *a)
 {
-	t_stack *b;
+	t_stack	*b;
+	size_t	count_rot;
+	size_t	limit;
+
+	int temp;
 	b = create_stack(a->size);
-	// pa(b, a);
-	printf("%d", b->data[0]);
-	free(b);
+	while (a->size >= 1)
+	{
+		temp = a-> data[0];
+		count_rot = 0;
+		limit = b->size;
+// ============================= Debug Traces =====================
+		printf("Stack A:");
+		for (int i = 0; i < a->size; i++)
+			printf(" %d", a->data[i]);
+		printf("\nStack B:");
+		for (int i = 0; i < b->size; i++)
+			printf(" %d", b->data[i]);
+		printf("\nTemp value: %d\n", temp);
+// ============================= Debug =====================
+		while(b->size > 0 && a->data[0] < b->data[0] && count_rot < limit)
+		{
+			rb(b);
+			count_rot++;
+		}
+		pb(b, a);
+		while (count_rot > 0)
+		{
+			rrb(b);
+			count_rot--;
+		}
+
+	}
+	while(b->size > 0)
+		pa(a, b);
+	clear_stack(b);
 }
 
 #ifdef TEST
 
-/*
-
-gcc -DTEST srcs/insertion_sort.c srcs/push.c srcs/load_stack.c srcs/clear_stack.c srcs/is_valid_int.c srcs/error.c srcs/ft_strcmp.c utils/display_stacks.c utils/putstack.c -L./Libft -lft -I./include -o test
-
-*/
+// gcc -DTEST $(ls srcs/*.c | grep -v main.c) utils/*.c -L./Libft -lft -I./include -o test
 
 int main(void)
 {
 	t_stack *a;
-	char *nums[] = {"1", "4", "7", "5", "2", NULL};
-
-	a = get_stack(5, nums);
+	char *nums[] = {"2", "1", "3", "6", "5", "8", NULL};
+	// char *nums[] = { "1", "2", "3", "5", "6", "8", NULL};
+	a = get_stack(6, nums);
 	insertion_sort(a);
+	printf("\nFinal stack A:");
+	for(int i = 0; i < a->size; i++)
+		printf(" %d", a->data[i]);
 	clear_stack(a);
+
 	return (0);
 }
 
