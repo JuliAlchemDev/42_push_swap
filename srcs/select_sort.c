cfb206231f020e432ea4e05ddbb7f4b20b0ac496
Julia Alkhimova <devalchem@gmail.com>
2026-07-06 17:41:19 +0200

    feat: initialize bench_data struct and call ft_putbench for --bench flag support

--- srcs/select_sort.c
+++ srcs/select_sort.c
@@ -18,8 +18,24 @@ void	select_sort(t_stack *a, t_stack *b, char strategy[], int bench_flag)
 
 	bench_data.disorder = compute_disorder(a);
 	bench_data.strategy = strategy;
+	// Extract init bench_data
+	bench_data.total_ops = 0;
+	bench_data.sa = 0;
+	bench_data.sb = 0;
+	bench_data.ss = 0;
+	bench_data.pa = 0;
+	bench_data.pb = 0;
+	bench_data.ra = 0;
+	bench_data.rb = 0;
+	bench_data.rr = 0;
+	bench_data.rra = 0;
+	bench_data.rrb = 0;
+	bench_data.rrr = 0;
+
 	if ((ft_strcmp(strategy, "--simple") == 0)
 		|| ((ft_strcmp(strategy, "--adaptive") == 0)
 			&& bench_data.disorder <= 0.2))
-		insertion_sort(a, b, bench_flag, &bench_data);
+		insertion_sort(a, b, &bench_data);
+	if (bench_flag)
+		ft_putbench(&bench_data);
 }
