bdbca2f00d36e480813881c76d35ae5bedd35db9
Andrea Serio on Comet <andrea.serio90@gmail.com>
2026-07-21 18:08:07 +0200

    chore: * norminette, * minor cosmetic.

--- srcs/ft_putbench.c
+++ srcs/ft_putbench.c
@@ -6,25 +6,22 @@
 /*   By: aserio <aserio@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 18:03:20 by aserio            #+#    #+#             */
-/*   Updated: 2026/07/09 15:13:22 by aserio           ###   ########.fr       */
+/*   Updated: 2026/07/20 17:42:37 by aserio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-static void count_total(t_context *ctx)
+static	void	count_total(t_context *ctx)
 {
 	ctx->total_ops += ctx->sa;
 	ctx->total_ops += ctx->sb;
 	ctx->total_ops += ctx->ss;
-
 	ctx->total_ops += ctx->pa;
 	ctx->total_ops += ctx->pb;
-
 	ctx->total_ops += ctx->ra;
 	ctx->total_ops += ctx->rb;
 	ctx->total_ops += ctx->rr;
-
 	ctx->total_ops += ctx->rra;
 	ctx->total_ops += ctx->rrb;
 	ctx->total_ops += ctx->rrr;
@@ -32,14 +29,12 @@ static void count_total(t_context *ctx)
 
 void	ft_putbench(t_context *ctx)
 {
-
-	
 	ft_putdisorder(ctx->disorder);
 	ft_printf("[bench] strategy: %s\n", ctx->strategy);
 	count_total(ctx);
 	ft_printf("[bench] total_ops: %d\n", ctx->total_ops);
 	ft_printf("[bench] sa: %d sb: %d ss: %d", ctx->sa, ctx->sb, ctx->ss);
-	ft_printf(" pa: %d pb: %d\n",  ctx->pa, ctx->pb);
+	ft_printf(" pa: %d pb: %d\n", ctx->pa, ctx->pb);
 	ft_printf("[bench] ra: %d rb: %d rr: %d", ctx->ra, ctx->rb, ctx->rr);
 	ft_printf(" rra: %d rrb: %d rrr: %d\n", ctx->rra, ctx->rrb, ctx->rrr);
 }
--- utils/display_stacks.c
+++ utils/display_stacks.c
@@ -6,7 +6,7 @@
 /*   By: aserio <aserio@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/28 12:27:16 by aserio            #+#    #+#             */
-/*   Updated: 2026/07/09 15:17:21 by aserio           ###   ########.fr       */
+/*   Updated: 2026/07/21 15:30:50 by aserio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	display_stacks(t_context *ctx)
 {
 	ft_putendl_fd("--------------------------------------------", 1);
-	ft_putstr_fd("a - size = ", 1);
+	ft_putstr_fd("a.size = ", 1);
 	ft_putnbr_fd(ctx->a->size, 1);
 	ft_putchar_fd('\n', 1);
 	putstack(ctx->a);
-	ft_putstr_fd("b - size = ", 1);
+	ft_putstr_fd("b.size = ", 1);
 	ft_putnbr_fd(ctx->b->size, 1);
 	ft_putchar_fd('\n', 1);
 	putstack(ctx->b);
