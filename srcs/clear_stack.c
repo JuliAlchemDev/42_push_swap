a2769594eabf80ae79583f549fbc50516e165865
Andrea-S90 <andrea.serio90@gmail.com>
2026-07-27 16:52:48 +0200

    fix : return ; instead of return (NULL) in void function.

--- srcs/clear_stack.c
+++ srcs/clear_stack.c
@@ -6,7 +6,7 @@
 /*   By: aserio <aserio@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 18:36:19 by aserio            #+#    #+#             */
-/*   Updated: 2026/07/27 16:51:30 by aserio           ###   ########.fr       */
+/*   Updated: 2026/07/27 16:52:14 by aserio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	clear_stack(t_stack *stack)
 {
 	if (!stack)
-		return (NULL);
+		return ;
 	if (stack->data)
 		free(stack->data);
 	free(stack);
