195e785d77a187a8a1604b63742d7bf4e02ac60a
Andrea Serio on Comet <andrea.serio90@gmail.com>
2026-08-03 20:50:18 +0200

    fix: update quick_sort to remove redundant stack display calls

--- srcs/quick_sort.c
+++ srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: aserio <aserio@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 03:38:27 by aserio            #+#    #+#             */
-/*   Updated: 2026/08/03 12:00:49 by aserio           ###   ########.fr       */
+/*   Updated: 2026/08/03 20:49:54 by aserio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,5 @@ float	avg_pivot(t_stack *stack, size_t size)
 
 void	quick_sort(t_context *ctx)
 {
-	display_stacks(ctx);
 	quick_sort_a(ctx, (size_t) ctx->a->size);
-	display_stacks(ctx);
 }
