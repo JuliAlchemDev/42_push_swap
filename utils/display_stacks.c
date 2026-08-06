289e2b94d398dfdb8a63356b518078c0812507ac
aserio <andrea.serio90@gmail.com>
2026-08-06 20:10:07 +0200

    fix: remove debug_flag from context and related references

--- utils/display_stacks.c
+++ utils/display_stacks.c
@@ -6,7 +6,7 @@
 /*   By: aserio <aserio@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/28 12:27:16 by aserio            #+#    #+#             */
-/*   Updated: 2026/08/03 16:59:00 by aserio           ###   ########.fr       */
+/*   Updated: 2026/08/06 20:09:13 by aserio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	display_stacks(t_context *ctx)
 {
-	if (ctx->debug_flag)
+	if (1)
 	{
 		ft_putendl_fd("--------------------------------------------", 1);
 		ft_putstr_fd("a.size = ", 1);
